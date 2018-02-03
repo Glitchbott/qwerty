a0762f0b324c13b6bff5586abeb118648f868259
Any <glitchbot@yandex.ru>
2018-02-03 11:55:19 +0700

    Modificate code classs

---
+++ SimpleClassExamplе/SimpleClassExamplе/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExamplе
+{
+    class Car
+    {
+        public string petName;
+        public int currSpeed;
+
+        public void PrintState()
+        {
+            Console.WriteLine("{0} is going {1} MPH.", petName, currSpeed);
+        }
+        public void SpeedUp (int delta)
+        {
+            currSpeed += delta;
+        }
+    }
+}
--- SimpleClassExamplе/SimpleClassExamplе/Program.cs
+++ SimpleClassExamplе/SimpleClassExamplе/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace SimpleClassExamplе
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
+
         }
     }
 }
