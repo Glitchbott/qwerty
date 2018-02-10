25ea34f9b21b0ede5f29214cbc8f72443b911d52
Any <glitchbot@yandex.ru>
2018-02-10 09:35:28 +0700

    Defined Static Class

---
+++ simpleUtilityClass/simpleUtilityClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleUtilityClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******Fun whith Static Classes  *****\n");
+            //РАБОТАЕТ
+            TimeUtilClass.PrintDate();
+            TimeUtilClass.PrintTime();
+            //НЕ РАБОТАЕТ
+          //  TimeUtilClass u = new TimeUtilClass();
+          //  Console.ReadLine();
+                
+        }
+
+    }
+}
