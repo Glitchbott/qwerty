284ed25a2a1fea3f9daa987ce09ad835f1a6f1a7
Any <glitchbot@yandex.ru>
2018-02-17 12:59:23 +0700

    constant data

---
+++ ConstData/ConstData/MyMathClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstData
+{
+    class MyMathClass
+    {
+        public const double PI = 3.14;
+    }
+
+   
+    }
+
---
+++ ConstData/ConstData/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun Whith Const *****\n");
+            Console.WriteLine("The value fo PI is: ", MyMathClass.PI);
+
+            Console.ReadLine();
+
+        }
+    }
+}
