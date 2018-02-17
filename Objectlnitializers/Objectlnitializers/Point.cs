1f0bb36081a2aeb8e9a9c2cb9aa442d0ef1de007
Any <glitchbot@yandex.ru>
2018-02-17 12:47:38 +0700

    Object initialization

---
+++ Objectlnitializers/Objectlnitializers/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objectlnitializers
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Color;
+        public Point (int xVal, int yVal, string color = " ")
+        {
+            X = xVal;
+            Y = yVal;
+            Color = color;
+        }
+        public Point() { }
+        public void DisplayStats()
+        {
+            Console.WriteLine("[{0}, {1}] {2} ", X, Y, Color );
+        }
+       
+
+    }
+}
---
+++ Objectlnitializers/Objectlnitializers/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objectlnitializers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun With Object Init Syntax *****\n");
+
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+
+            Point anotherPoint = new Point(20, 20, " Red ");
+            anotherPoint.DisplayStats();
+
+            Point finalPoint = new Point{X = 30, Y = 30 };
+            finalPoint.DisplayStats();
+            Console.ReadLine();
+       
+
+        }
+    }
+}
